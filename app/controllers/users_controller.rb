6e272c61ea7b93d471e217d5301316b8a9ce7472
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-16 22:42:16 +0200

    Fix typos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
       MailGibbon.new(params[:user][:email]).add_user
       flash["success"] = "Bienvenue ! Vous allez recevoir un email de confirmation."
     else
-      flash["danger"] = "Vous n'avez pas été ajouté à notre mailing liste."
+      flash["danger"] = "Vous n'avez pas été ajouté à notre newletter. Le mail ne doit pas être valide ou déjà pris."
     end
     redirect_to root_path
   end
