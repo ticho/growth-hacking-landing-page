bbf6f67b89c34a6f2389719c0c53715ac9ed30dc
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-16 15:00:41 +0200

    Add_user form done

--- app/services/mail_gibbon.rb
+++ app/services/mail_gibbon.rb
@@ -5,12 +5,14 @@ class MailGibbon
   end
 
   def add_user
-    gibbon = Gibbon::Request.new(api_key: Rails.application.credentials.mailchimp['api_key'])
+    puts "ozo"
+    puts Rails.application.credentials.mailchimp[:api_key]
+    gibbon = Gibbon::Request.new(api_key: Rails.application.credentials.mailchimp[:api_key])
     gibbon.lists('9b2aa33971').members.create(body: {email_address: @email, status: "subscribed"})
   end
 
   def newletter
-    gibbon = Gibbon::Request.new(api_key: Rails.application.credentials.mailchimp['api_key'])
+    gibbon = Gibbon::Request.new(api_key: Rails.application.credentials.mailchimp[:api_key])
     gibbon.campaigns('mailchimp0ba270124b2035bcd4642bcff.02f0a0bb98').actions.send.create
   end
 end
